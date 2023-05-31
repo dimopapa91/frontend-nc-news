ca323942c20cd4b92b2e770aa70ca2351366c7e7
dimopapa91 <64607400+dimopapa91@users.noreply.github.com>
2023-05-31 10:47:22 +0100

    finished ticket 5

--- src/Components/ArticleList.jsx
+++ src/Components/ArticleList.jsx
@@ -24,11 +24,11 @@ function ArticleList() {
 
     //Rendering State
     return (
-        <main classname="articles-page">
+        <main className="articles-page">
             <h2>Available Articles</h2>
             <ul>
                 {articles.map(({ article_id, title, body, article_img_url }) => {
-                    return <li key={ article_id }><Link to={ `/articles/${ article_id }` }>{ title }</Link>
+                    return <li key={ article_id }><Link to={ `/articlesById/${ article_id }` }>{ title }</Link>
                     <br></br>
                     <img src={ article_img_url } alt={title} className="article-image"></img>
                     <p>{ body }</p>
