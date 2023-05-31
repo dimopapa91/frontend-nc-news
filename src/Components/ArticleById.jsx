b8070f953001dd04f7e2cee02f7c08780e213d89
dimopapa91 <64607400+dimopapa91@users.noreply.github.com>
2023-05-31 11:55:49 +0100

    updated ticket 5

---
+++ src/Components/ArticleById.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { fetchArticleById } from "../../utils";
+import { useParams } from "react-router-dom";
+
+
+function ArticleById() {
+    const [article, setArticle] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const {article_id} = useParams();
+
+    useEffect(() => {
+        fetchArticleById(article_id)
+        .then(({article}) => {
+            setArticle(article);
+            setIsLoading(false);
+        })
+    }, []);
+
+    if (isLoading) {
+        return (
+            <p className="loading-state">Loading...</p>
+        )
+    }
+
+
+    return (
+        <main classname="article-id">
+            <h2>Article</h2>
+            <p className="article-title">{article.title}</p>
+            <p className="article-topic">{article.topic}</p>
+            <p className="article-author">{article.author}</p>
+            <p className="article-body">{article.body}</p>
+            <img
+                src={article.article_img_url}
+                alt={article.title}
+                className="article-img">
+            </img>
+            <p className="article-votes">Votes: {article.votes}</p>
+        </main>
+    )
+}
+
+
+export default ArticleById;
